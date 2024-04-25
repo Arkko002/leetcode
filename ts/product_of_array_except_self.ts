5944d83d9ebef590959d30cb5e21036303a1289b
Arkko <arkko002@gmail.com>
2024-04-25 22:53:00 +0200

    Add product of array except self solution

--- ts/product_of_array_except_self.ts
+++ ts/product_of_array_except_self.ts
@@ -1,39 +1,28 @@
-function productExceptSelf(nums: number[]): number[] {
-  // let matrix: number[][] = [];
-  let product: number = 1;
-  for (let i = 0; i < nums.length; i++) {
-    // matrix[i] = product;
-    product *= nums[i];
+const productExceptSelf = (nums: number[]): number[] => {
+  const n: number = nums.length;
+
+  console.log(`input: ${JSON.stringify(nums)}`);
+  // for a given i, prefix[i] stores product of all previous elements, suffix[i] stores product of all next elements
+  const prefix: number[] = new Array(n).fill(1);
+  const suffix: number[] = new Array(n).fill(1);
+
+  for (let i = 1; i < n; i++) {
+    prefix[i] = prefix[i - 1] * nums[i - 1];
+    console.log(`prefix: ${JSON.stringify(prefix)}`);
   }
 
-  const result: number[] = [];
-  for (const num of nums) {
-    result.push(divideWithSubstraction(product, num));
+  for (let i = n - 2; i >= 0; i--) {
+    suffix[i] = suffix[i + 1] * nums[i + 1];
+    console.log(`suffix: ${JSON.stringify(suffix)}`);
   }
 
-  return result;
-}
-
-const divideWithSubstraction = (a: number, b: number): number => {
-  if (a === 0 || b === 0) {
-    return 0;
+  const answer: number[] = [];
+  for (let i = 0; i < n; i++) {
+    console.log(`answer: ${JSON.stringify(answer)}`);
+    answer[i] = prefix[i] * suffix[i];
   }
 
-  let a_abs = Math.abs(a);
-  let b_abs = Math.abs(b);
-
-  let result: number = 0;
-  while (a_abs >= b_abs) {
-    a_abs -= b_abs;
-    result++;
-  }
-
-  if (0 > a || 0 > b && !(0 > a && 0 > b)) {
-    result = -result;
-  }
-
-  return result;
+  return answer;
 };
 
-[1, 2, 2, 2, 2, 2, 2, 2];
-[1, 2, 4, 8, 16, 32, 64, 128];
+console.log(`answer: ${JSON.stringify(productExceptSelf([1, 2, 3, 4]))}`);
